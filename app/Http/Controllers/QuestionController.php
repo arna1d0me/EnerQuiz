47e772e9d314514411b36b179415d234a1da6a8c
Arnau Domènech Bravin <arnaudome@gmail.com>
2023-10-15 18:21:58 +0200

    feat (Question:all() route defined)

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -12,7 +12,8 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
+        $questions = Question::all();
+        return response()->json($questions);
     }
 
     /**
